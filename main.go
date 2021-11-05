3690ff7977b5e288027bcb95c7d6eeea542051b9
Gabriel Tetzlaf Mansano <tetz.gabriel@gmail.com>
2021-11-05 08:34:30 -0300

    Feature: Getting Random Countries

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"time"
@@ -20,7 +21,7 @@ type Credentials struct {
 	AccessTokenSecret string
 }
 
-type country struct {
+type Country struct {
 	Name struct {
 		Common string `json:"common"`
 	} `json:"name"`
@@ -59,10 +60,78 @@ func main() {
 		ConsumerSecret:    os.Getenv("CONSUMER_SECRET"),
 	}
 
-	log.Printf("Success!\n")
+	log.Printf("Preparing request...\n")
+	url := os.Getenv("COUNTRY_API_URL")
+	countryclient, req := prepareRequest(url)
 
-	url := "https://restcountries.com/v3.1/all"
+	log.Printf("Calling countries API...\n")
+	res := getCountries(countryclient, req)
 
+	log.Printf("Parsing Data...\n")
+	countries := parseResponse(res)
+	fmt.Println(countries[0])
+
+	log.Printf("Getting Twitter client...\n")
+	client, err := getClient(&creds)
+	if err != nil {
+		log.Println("Error getting Twitter Client")
+		log.Println(err)
+	}
+
+	countryToTweet := getRandomCountry(countries)
+	clearRequestData(res)
+	tweet(client, &countryToTweet)
+}
+
+func getRandomCountry(countries []Country) Country {
+	num := getRandomInt()
+
+	return countries[num]
+}
+
+func getRandomInt() int {
+	rand.Seed(time.Now().UnixNano())
+	min := 0
+	max := 248
+
+	return rand.Intn(max-min+1) + min
+}
+
+func tweet(client *twitter.Client, country *Country) {
+	tweet, _, err := client.Statuses.Update("Country: "+country.Name.Common+"  -  Capital City: "+country.Capital[0], nil)
+	if err != nil {
+		log.Printf("Error tweeting!")
+		log.Fatal(err)
+	}
+
+	log.Printf("Success tweeting!")
+	log.Printf("%+v\n", tweet)
+}
+
+func parseResponse(res *http.Response) []Country {
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	fmt.Println(res)
+
+	countries := []Country{}
+	jsonErr := json.Unmarshal(body, &countries)
+
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+	return countries
+}
+
+func clearRequestData(res *http.Response) {
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+}
+
+func prepareRequest(url string) (http.Client, *http.Request) {
 	countryclient := http.Client{
 		Timeout: time.Second * 2,
 	}
@@ -72,46 +141,15 @@ func main() {
 		log.Fatal(countryerr)
 	}
 
-	req.Header.Set("User-Agent", "spacecount-tutorial")
+	req.Header.Set("User-Agent", "capitaisBot")
 
+	return countryclient, req
+}
+
+func getCountries(countryclient http.Client, req *http.Request) *http.Response {
 	res, getErr := countryclient.Do(req)
 	if getErr != nil {
 		log.Fatal(getErr)
 	}
-
-	if res.Body != nil {
-		defer res.Body.Close()
-	}
-
-	body, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
-
-	fmt.Println(res)
-
-	countries := []country{}
-	jsonErr := json.Unmarshal(body, &countries)
-
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
-
-	fmt.Println(countries[0])
-
-	client, err := getClient(&creds)
-	if err != nil {
-		log.Println("Error getting Twitter Client")
-		log.Println(err)
-	}
-
-	tweet, resp, err := client.Statuses.Update("A Test Tweet from a new Bot I'm building!", nil)
-
-	if resp.StatusCode == 200 && err != nil {
-		log.Printf("Success tweeting!")
-		log.Printf("%+v\n", tweet)
-	} else {
-		log.Printf("Error tweeting!")
-		log.Println(err)
-	}
+	return res
 }
